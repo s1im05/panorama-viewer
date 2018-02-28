c0fcf95833c43659a4993afc909eb696445f5bf2
Vadim Suleimanov <visulej2@mts.ru>
2018-02-28 11:20:21 +0300

    Toggle FullScreen button

--- src/main.ts
+++ src/main.ts
@@ -17,5 +17,45 @@ import {Panorama} from './panorama';
         panorama.isAnimated = event.target['checked'];
     });
 
-    window.addEventListener('resize', panorama.onResize.bind(panorama));
+    document.getElementById('btn_fullscreen').addEventListener('click', (event) => {
+        if (document.documentElement.requestFullscreen) {
+            document.documentElement.requestFullscreen();
+        } else if (document.documentElement.webkitRequestFullScreen) {
+            document.documentElement.webkitRequestFullScreen();
+        } else if (document.documentElement['mozRequestFullScreen']) {
+            document.documentElement['mozRequestFullScreen']();
+        }
+    });
+
+    document.getElementById('btn_fullscreen_exit').addEventListener('click', (event) => {
+        if (document.exitFullscreen) {
+            document.exitFullscreen();
+        } else if (document.webkitExitFullscreen) {
+            document.webkitExitFullscreen();
+        } else if (document['mozExitFullscreen']) {
+            document['mozExitFullscreen']();
+        }
+    });
+
+    window.addEventListener('resize', () => {
+        panorama.onResize();
+
+        let isFullScreen;
+        if (document.webkitIsFullScreen !== undefined) {
+            isFullScreen = document.webkitIsFullScreen;
+        } else if (document['mozFullScreen'] !== undefined) {
+            isFullScreen = document['mozFullScreen'];
+        } else if (document['fullscreen'] !== undefined) {
+            isFullScreen = document['fullscreen'];
+        }
+        if (isFullScreen !== undefined) {
+            if (isFullScreen) {
+                document.getElementById('btn_fullscreen').classList.add('hide');
+                document.getElementById('btn_fullscreen_exit').classList.remove('hide');
+            } else {
+                document.getElementById('btn_fullscreen').classList.remove('hide');
+                document.getElementById('btn_fullscreen_exit').classList.add('hide');
+            }
+        }
+    });
 })();
